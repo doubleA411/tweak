d3a901130bed430546e03e61d130653eb80e2083
Aakash <56353087+xii-aakash@users.noreply.github.com>
2023-06-10 19:26:55 +0530

    shorthand fixed

--- app/FAQ/page.tsx
+++ app/FAQ/page.tsx
@@ -8,12 +8,13 @@ import {
 export default function FAQPage() {
   return (
     <div className=" flex flex-col justify-center items-center mx-32">
-      <p className="mt-10 text-3xl font-extrabold mb-10">FAQ</p>
+      <p className="my-10 text-3xl font-extrabold ">FAQ</p>
       <Accordion type="single" collapsible className=" w-[500px]">
         <AccordionItem value="item-1">
           <AccordionTrigger>How to download and install?</AccordionTrigger>
           <AccordionContent>
-            The download button in the home page will redirct you to the download link.
+            The download button in the home page will redirct you to the
+            download link.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
@@ -25,11 +26,14 @@ export default function FAQPage() {
         <AccordionItem value="item-3">
           <AccordionTrigger>Is this tool paid?</AccordionTrigger>
           <AccordionContent>
-            The answer is no and yes, If you want the product at no cost you can. Anyway if you want to tip me something i'd appreciate it.
+            The answer is no and yes, If you want the product at no cost you
+            can. Anyway if you want to tip me something i'd appreciate it.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-4">
-          <AccordionTrigger>Do we have to sign up or anything ?</AccordionTrigger>
+          <AccordionTrigger>
+            Do we have to sign up or anything ?
+          </AccordionTrigger>
           <AccordionContent>
             No need, Only mail id is enough for you to collect your purchase.
           </AccordionContent>
--- app/page.tsx
+++ app/page.tsx
@@ -35,31 +35,7 @@ export default function IndexPage() {
           )
         })}
 
-        {/* <div className="flex gap-2 justify-center items-center mt-8">
-          <img src="tweak.png" alt="" width={"30px"} />
-          <p className=" font-extrabold text-3xl">Tweak</p>
-        </div>
-        <div className="flex gap-6 mt-7 justify-center items-center">
-          <Link href={"https://twitter.com/aakashsuresh62"}>
-            <p>Contact</p>
-          </Link>
-          <Link href={"/FAQ"}>
-            <p>FAQ</p>
-          </Link>
-          <Link href={"/terms"}>
-            <p>Terms & Conditions</p>
-          </Link>
-          <Link href={"/policy"}>
-            <p>Privacy Policy</p>
-          </Link>
-        </div>
-        <Link href={"https://portfolio-aa.vercel.app/"}>
-          <div className=" flex gap-2">
-            <p className=" text-muted-foreground">Made by Minimal Mind</p>
-            <img src="logo.png" alt="logo" width={"30px"} />
-          </div>
-        </Link> */}
-      
+     
       </div>
     </section>
   )
